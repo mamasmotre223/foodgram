41707e4c707dee1c4683b906e768ca2ec628a657
Nikita Lebedev <mamasmotre223@gmail.com>
2026-05-14 17:48:08 +0300

    style: apply isort ordering to satisfy flake8-isort

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
-﻿from django.urls import include, path
+﻿from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
-
 
 router = DefaultRouter()
 router.register("users", UserViewSet, basename="users")

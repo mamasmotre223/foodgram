09ca4b3d5a1962c22d94e3e8a919b467294f2940
Nikita Lebedev <mamasmotre223@gmail.com>
2026-05-14 18:27:01 +0300

    ci: make flake8 config explicit for backend lint step

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,10 +1,8 @@
-﻿from django.conf import settings
+﻿from api.views import ShortLinkRedirectView
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from api.views import ShortLinkRedirectView
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,5 @@
 ﻿from django.contrib import admin
 from django.db.models import Count
-
 from recipes.models import (
     Favorite,
     Ingredient,
